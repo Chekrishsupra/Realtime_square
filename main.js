4d0000f46531cab8ab6e34b8dfa3ec99d4c58bfe
Chekrishsuper <78082844+Chekrishsupra@users.noreply.github.com>
2021-08-11 19:45:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+function setup() {
+video = createCapture(VIDEO);
+video.size(550,500);
+
+canvas = createCanvas(550,400);
+canvas.position(560,150);
+
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function draw() {
+    background('#4d4949');
+    document.getElementById("square_sides").innerHTML = "Width and Height of the square will be =  " + difference + "px";
+    fill("#f00e0e");
+    stroke("#f00e0e");
+    square(noseX,noseY,difference);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results); 
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x:" + noseX + ",nose y:" + noseY);
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left wrist x:" + leftWristX + ",Right wrist x:" + rightWristX + "and difference:" + difference);
+    }
+}
